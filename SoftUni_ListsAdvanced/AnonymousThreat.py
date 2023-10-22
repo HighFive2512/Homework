5af039ee4d9c50b10c8258e7f1b79c391c33f6b3
HighFive2512 <highfive2512@yahoo.com>
2023-10-22 10:30:20 +0300

    new hw

--- SoftUni_ListsAdvanced/AnonymousThreat.py
+++ SoftUni_ListsAdvanced/AnonymousThreat.py
@@ -1,35 +1,32 @@
+from math import ceil
+
 def merge(string,parameters):
     param1, param2 = parameters
     param1 = int(param1)
     param2 = int(param2)
-    if int(param2) > len(string):
+    if param1 < 0:
+        param1 = 0
+    if int(param2) >= len(string):
         param2 = len(string)
-        if param1 == param2:
-            return string
-    combined = string[param1:param2]
-    combined = ''.join(combined)
-    asd = string[0:param1] + [combined] + string[param2:]
-    return asd
+    combined = ''.join(string[param1:param2+1])
+    string[param1:param2+1] = [combined]
+    return string
 def divide(string,parameters):
     param1, param2 = parameters
     param1 = int(param1)
     param2 = int(param2)
-    while True:
-        selected_string = string[param1]
-        tempolist = []
-        selected_index = string.index(selected_string)
-        previndex = 0
-        divide_by = len(string) // param2
-        for each_index in range(2,len(selected_string),1+divide_by):
-            if each_index+1 == param2+1:
-                if len(selected_string) % 2 == 1:
-                    tempolist[previndex] = selected_string[previndex:each_index+1]
-                else:
-                    tempolist[-1] = selected_string[previndex:each_index]
-            tempolist.append(selected_string[previndex:each_index])
-            previndex = each_index
-        break
+    selected_string = string[param1]
+    selected_index = string.index(selected_string)
+    tempolist = []
+    divide_by = len(selected_string) // param2
+    for eachindx in range(param2):
+        if eachindx == param2 -1:
+            tempolist.append(selected_string[eachindx*divide_by:])
+        else:
+            tempolist.append(selected_string[eachindx * divide_by:(eachindx + 1) * divide_by])
+
     string.pop(selected_index)
+
     for i in range(len(tempolist)):
         string.insert(selected_index+i,tempolist[i])
     return string
