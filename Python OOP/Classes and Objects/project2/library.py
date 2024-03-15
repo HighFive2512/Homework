ab567ad830b597a3b91052c74a7733c524dadbb2
HighFive2512 <highfive2512@yahoo.com>
2024-03-15 21:25:58 +0200

    testing stuff

--- Python OOP/Classes and Objects/project2/library.py
+++ Python OOP/Classes and Objects/project2/library.py
@@ -6,5 +6,20 @@ class Library:
         self.books_available = {}
         self.rented_books = {}
     def get_book(self,author:str,book_name:str,days_to_return:int,user:User):
-        if book_name in self.books_available.values():
-            self.books_available.pop()
+        for eachkey, eachval in self.books_available.items():
+            if book_name in eachval:
+                eachval.remove(book_name)
+                self.rented_books[user][book_name] = days_to_return
+                return f"{book_name} successfully rented for the next {days_to_return} days!"
+            else:
+                return f'The book "{book_name}" is already rented and will be available in {days_to_return} days!'
+    def return_book(self,author:str, book_name:str, user: User):
+        if book_name in self.rented_books[user]:
+            self.rented_books[user].pop(book_name)
+            if author not in self.books_available:
+                self.books_available[author] = book_name
+            else:
+                self.books_available[author] += book_name
+        else:
+            return f"{user} doesn't have this book in his/her records!"
+
