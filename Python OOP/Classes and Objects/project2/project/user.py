5536e77c64e8b5e13dcbca07bbec1bda45cc2719
HighFive2512 <highfive2512@yahoo.com>
2024-03-19 12:12:14 +0200

    testing stuff

---
+++ Python OOP/Classes and Objects/project2/project/user.py
@@ -0,0 +1,11 @@
+from typing import List
+class User:
+    def __init__(self,user_id:int,username:str):
+        self.books: List[str]= []
+        self.user_id = user_id
+        self.username = username
+    def info(self) -> str:
+        return ', '.join(sorted(self.books)) or "[]"
+
+    def __str__(self):
+        return f'{self.user_id}, {self.username}, {self.info()}'
